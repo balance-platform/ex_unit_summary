fbe4251b5318e19c0a0488a9e75fcde012e32bdf
Дмитрий Шпагин <dmitry.shpagin@balance-pl.ru>
2026-01-12 18:49:50 +0300

    up 0.2.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ExUnitSummary.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/balance-platform/ex_unit_summary"
-  @version "0.1.0"
+  @version "0.2.0"
 
   @spec project() :: list()
 
